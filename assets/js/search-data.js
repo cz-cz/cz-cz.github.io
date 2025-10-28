25236189ec12efe65ce94eeb9e52815d0dc4720f
cz-cz <67094803+cz-cz@users.noreply.github.com>
2025-10-28 11:48:34 +0000

    Deploying to gh-pages from @ cz-cz/cz-cz.github.io@c1a5e02440f0fa2a324e65b76a4c339edbfdeb9a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,14 +3,14 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-",
+    title: "",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
-          title: "cv",
+  },{id: "nav-简历-cv",
+          title: "简历-cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
